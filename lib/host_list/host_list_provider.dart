37863a5719c690a93d191c05ae9850ca58cd0b3f
”SulingXu“ <xusuling2020@gmail.com>
2021-07-26 11:23:54 +0200

    Add host list provider

---
+++ lib/host_list/host_list_provider.dart
@@ -0,0 +1,21 @@
+import 'host.dart';
+
+// Provide a list of hosts and APIs to manipulate the host list.
+class HostListProvider {
+
+  // internal list of hosts, can be changed by api.
+  List<Host> _hosts = [
+    Host("Suling Xu"),
+    Host("Nan Wang")
+  ];
+
+  // host list provider api: provide a list of hosts
+  List<Host> provideHosts() {
+    return _hosts;
+  }
+
+  // API to add a new host
+  void addHost(Host host) {
+    _hosts.add(host);
+  }
+}
