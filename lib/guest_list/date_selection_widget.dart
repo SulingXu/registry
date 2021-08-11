4fec64e4e823c7338621c6455b8f47d4a1b7fbbf
”SulingXu“ <xusuling2020@gmail.com>
2021-08-11 11:40:23 +0200

    change the name of  GuestSelectDate to DateSelectionDate

--- lib/guest_list/date_selection_widget.dart
+++ lib/guest_list/date_selection_widget.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
 import 'guest_info_input_widget.dart';
 
-class GuestSelectDate extends StatefulWidget {
-  const GuestSelectDate({Key? key, required this.completion}) : super(key: key);
+class DateSelectionWidget extends StatefulWidget {
+  DateSelectionWidget({Key? key, required this.completion}) : super(key: key);
   final VoidCallback completion;
+  DateTime selectedDate = DateTime.now();
   @override
-  _GuestSelectDateState createState() => _GuestSelectDateState();
+  _DateSelectionWidgetState createState() => _DateSelectionWidgetState();
 }
 
-class _GuestSelectDateState extends State<GuestSelectDate> {
-  DateTime _selectedDate = DateTime.now();
+class _DateSelectionWidgetState extends State<DateSelectionWidget> {
 
   //waits for the date selected by the user
-  _selectDate(BuildContext context) async {
+  void _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: _selectedDate,
+      initialDate: widget.selectedDate,
       firstDate: DateTime(2020),
       lastDate: DateTime(2025),
     );
-    if (picked != null && picked != _selectedDate)
+    if (picked != null && picked !=  widget.selectedDate)
       setState(() {
-        _selectedDate = picked;
+        widget.selectedDate = picked;
       });
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            text("${_selectedDate.toLocal()}".split(' ')[0]),
+            text('${ widget.selectedDate.toLocal()}'.split(' ')[0]),
             const SizedBox(width: 50),
             IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.calendar_today_outlined,
               ),
               iconSize: 28,
