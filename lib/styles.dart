deca2a1c6a1838843d03826d4351cffe35e10fd7
”SulingXu“ <xusuling2020@gmail.com>
2021-08-15 19:32:38 +0200

    Add guest_check_out_dialog and add two button style in styles.dart

--- lib/styles.dart
+++ lib/styles.dart
@@ -11,10 +11,46 @@ abstract class Styles {
       textStyle: const TextStyle(fontSize: 18), padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15)
   );
 
+  static ButtonStyle unCheckedOutbuttonColor = ElevatedButton.styleFrom(
+    primary: Colors.green, // background
+    onPrimary: Colors.white, // foreground
+  );
+
+  static ButtonStyle CheckedOutbuttonColor = ElevatedButton.styleFrom(
+    primary: Colors.grey, // background
+    onPrimary: Colors.black, // foreground
+  );
+
   static Widget text(String txtString) {
     return Text(
         txtString,
         style: Styles.textStyle
     );
   }
+
+  static Widget textRowWith(String prefix, String showString) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Styles.text(prefix),
+        const SizedBox(width: 10),
+        Styles.text(showString),
+      ],
+    );
+  }
+
+  static Widget paddingWithText(String txt) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: text(txt),
+    );
+  }
+
+  static Widget paddingWithTextRow(String prefix, String showString) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: textRowWith(prefix, showString),
+    );
+  }
+
 }
