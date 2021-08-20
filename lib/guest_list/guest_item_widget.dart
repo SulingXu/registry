62c75fc3645437572b83e67694123f282773cd10
”SulingXu“ <xusuling2020@gmail.com>
2021-08-20 19:48:22 +0200

    Add one more dateshowing style method, change navigating route, put guest item into card widget, add dependencies, put method compareTwoDate outside of the class

--- lib/guest_list/guest_item_widget.dart
+++ lib/guest_list/guest_item_widget.dart
@@ -3,10 +3,21 @@ import 'package:registry/guest_list/guest_check_out_alert_dialog.dart';
 import 'package:registry/guest_list/guest_status.dart';
 import 'package:registry/guest_list/guest.dart';
 import 'package:registry/styles.dart';
+import 'package:registry/host_list/host_list_provider.dart';
+import 'package:registry/guest_list/guest_list_provider.dart';
+import 'package:registry/guest_list/guest_list_widget.dart';
 
 class GuestItemWidget extends StatefulWidget {
-  GuestItemWidget({Key? key, required this.guest}) : super(key: key);
+  GuestItemWidget(
+      {Key? key,
+      required this.guest,
+      required this.guestListProvider,
+      required this.hostListProvider})
+      : super(key: key);
   Guest guest;
+  GuestListProvider guestListProvider;
+  HostListProvider hostListProvider;
+
   @override
   _GuestItemWidgetState createState() => _GuestItemWidgetState();
 }
@@ -18,62 +29,91 @@ class _GuestItemWidgetState extends State<GuestItemWidget> {
   final String _checkOutButtonTxt = 'Check out';
   final String _checkedOutButtonTxt = 'Checked out';
 
-  Widget checkOutButton () {
+  Widget checkOutButton() {
     return ElevatedButton(
-      style: widget.guest.hasCheckedOut? Styles.CheckedOutbuttonColor : Styles.unCheckedOutbuttonColor,
+      style: widget.guest.hasCheckedOut
+          ? Styles.CheckedOutbuttonColor
+          : Styles.unCheckedOutbuttonColor,
       onPressed: widget.guest.hasCheckedOut ? null : Function,
-      child: Styles.text(widget.guest.hasCheckedOut? _checkedOutButtonTxt: _checkOutButtonTxt),
+      child: Styles.text(
+          widget.guest.hasCheckedOut
+              ? _checkedOutButtonTxt
+              : _checkOutButtonTxt,
+          Styles.middleTextWithDefaultColor),
     );
   }
+
   void Function() {
-    widget.guest.checkOutTime = DateTime.now();//get current time when the checkOutButton is pressed
-    showDialog<void> (context: context,
-      builder: (BuildContext context) {
-        return GuestCheckOutAlertDialog(
-          guest: widget.guest,
-          checkOutTimeUpdate: () =>
-            setState(()  => widget.guest.hasCheckedOut = true)
-        );
-      });
+    widget.guest.checkOutTime =
+        DateTime.now(); //get current time when the checkOutButton is pressed
+    showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return GuestCheckOutAlertDialog(
+              guest: widget.guest,
+              guestListProvider: widget.guestListProvider,
+              hostListProvider: widget.hostListProvider,
+              checkOutTimeUpdate: () =>
+                  setState(() => widget.guest.hasCheckedOut = true));
+        });
   }
-  String _getFormattedTime(DateTime? time) {
+
+  String _getHrMin(DateTime? time) {
     if (time == null) {
       return '---';
     } else {
-      return "${time.hour.toString().padLeft(2,'0')}:${time.minute.toString().padLeft(2,'0')}:${time.second.toString().padLeft(2,'0')}";
+      return "${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}";
+    }
+  }
+
+  String _getMonDayHrMin(DateTime? time) {
+    if (time == null) {
+      return '---';
+    } else {
+      return "${time.month.toString().padLeft(2, '0')}-${time.day.toString().padLeft(2, '0')} ${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}";
     }
   }
 
   @override
   Widget build(BuildContext context) {
     String _guestName = widget.guest.firstName + ' ' + widget.guest.lastName;
-    String _status = (widget.guest.status == GuestStatus.play) ? 'play' : 'watch';
+    String _status =
+        (widget.guest.status == GuestStatus.play) ? 'play' : 'watch';
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Styles.paddingWithTextRow(_guestNameTextPrefix, _guestName),
-        Styles.paddingWithTextRow(_hostNameTextPrefix, widget.guest.chosenHostName ?? ' '),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Styles.textRowWith(_statusTextPrefix, _status),
-              const SizedBox(width: 50),
-              checkOutButton(),//checkOutButton
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Align(
-            alignment: Alignment.center,
-            child: Styles.text("From " + _getFormattedTime(widget.guest.checkInTime) + " to " + _getFormattedTime(widget.guest.checkOutTime),)
-          ),
-        ),
-        Divider(),
-      ],
-    );
+    return Card(
+        elevation: 4.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Styles.paddingWithTextRow(_guestNameTextPrefix, _guestName),
+            Styles.paddingWithTextRow(
+                _hostNameTextPrefix, widget.guest.chosenHostName ?? ' '),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Styles.textRowWith(_statusTextPrefix, _status),
+                  const SizedBox(width: 50),
+                  checkOutButton(), //checkOutButton
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Styles.text(
+                      "From  " +
+                          _getHrMin(widget.guest.checkInTime) +
+                          "  to  " +
+                          (compareTwoDate(widget.guest.checkInTime,
+                                  widget.guest.checkOutTime)
+                              ? _getHrMin(widget.guest.checkOutTime)
+                              : _getMonDayHrMin(widget.guest.checkOutTime)),
+                      Styles.middleTextWithDefaultColor)),
+            ),
+          ],
+        ));
   }
 }
