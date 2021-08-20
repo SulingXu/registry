ef153863a4cfc986a3e3fb851d5b8bed50eb7eaa
”SulingXu“ <xusuling2020@gmail.com>
2021-08-20 19:52:49 +0200

    Change navigating route, put guest item into card widget, reformat code, put method compareTwoDate outside of the class for reuse by other classes

--- lib/guest_list/guest_list_widget.dart
+++ lib/guest_list/guest_list_widget.dart
@@ -3,10 +3,30 @@ import 'package:registry/guest_list/guest_item_widget.dart';
 import 'package:registry/guest_list/guest_list_provider.dart';
 import 'package:registry/guest_list/guest.dart';
 import 'package:registry/guest_list/date_selection_widget.dart';
+import 'package:registry/host_list/host_list_provider.dart';
+import 'package:registry/host_list/host_list_widget.dart';
+
+bool compareTwoDate(DateTime? d1, DateTime? d2) {
+  if (d1 != null && d2 != null) {
+    if ((d1.year == d2.year) && (d1.month == d2.month) && (d1.day == d2.day)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
 
 class GuestListWidget extends StatefulWidget {
-  const GuestListWidget({Key? key, required this.guestListProvider}) : super(key: key);
+  const GuestListWidget(
+      {Key? key,
+      required this.guestListProvider,
+      required this.hostListProvider})
+      : super(key: key);
   final GuestListProvider guestListProvider;
+  final HostListProvider hostListProvider;
+
   @override
   _GuestListWidgetState createState() => _GuestListWidgetState();
 }
@@ -17,14 +37,11 @@ class _GuestListWidgetState extends State<GuestListWidget> {
   List<Guest> _filterGuestWithDate(DateTime selectedDate) {
     var i = 0;
     List<Guest> _selectedGuests = [];
-    DateTime currentDate = DateTime.now();
-    if(_compareTwoDate(selectedDate, currentDate)) {
-      return widget.guestListProvider.provideGuests();
-    }
-    while(i < widget.guestListProvider.provideGuests().length) {
-      if(widget.guestListProvider.provideGuests()[i].checkInTime != null) {
-        DateTime recordedDate = widget.guestListProvider.provideGuests()[i].checkInTime!;
-        if(_compareTwoDate(selectedDate, recordedDate)) {
+    while (i < widget.guestListProvider.provideGuests().length) {
+      if (widget.guestListProvider.provideGuests()[i].checkInTime != null) {
+        DateTime recordedDate =
+            widget.guestListProvider.provideGuests()[i].checkInTime!;
+        if (compareTwoDate(selectedDate, recordedDate)) {
           _selectedGuests.add(widget.guestListProvider.provideGuests()[i]);
         }
       }
@@ -33,45 +50,42 @@ class _GuestListWidgetState extends State<GuestListWidget> {
     return _selectedGuests;
   }
 
-  bool _compareTwoDate(DateTime d1, DateTime d2) {
-    if((d1.year == d2.year) && (d1.month == d2.month) && (d1.day == d2.day)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    List<Guest> _selectedGuests = List.from(_filterGuestWithDate(_selectedDate));
+    List<Guest> _selectedGuests =
+        List.from(_filterGuestWithDate(_selectedDate));
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Guest Records'), automaticallyImplyLeading: false),
-      body: Column(
-          children: [
-            DateSelectionWidget(dateChanged: (DateTime) =>
+      appBar: AppBar(
+          title: const Text('Guest Records'), automaticallyImplyLeading: false),
+      body: Column(children: [
+        DateSelectionWidget(
+            dateChanged: (DateTime) =>
                 setState(() => _selectedDate = DateTime)),
-            Divider(),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: _selectedGuests.length,
-                    itemBuilder: (context, index) {
-                      // Get a specific list
-                      final guest = _selectedGuests[index];
-                      // Return a list tile widget
-                      return GuestItemWidget(guest: guest);
-                    }
-                )
-            )
-          ]
-      ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: _selectedGuests.length,
+                itemBuilder: (context, index) {
+                  // Get a specific list
+                  final guest = _selectedGuests[index];
+                  // Return a list tile widget
+                  return GuestItemWidget(
+                      guest: guest,
+                      guestListProvider: widget.guestListProvider,
+                      hostListProvider: widget.hostListProvider);
+                }))
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.popUntil(context, (Route<dynamic> route) => route.isFirst);
+          Navigator.push(
+              context,
+              new MaterialPageRoute<void>(
+                  builder: (context) => HostListWidget(
+                      hostListProvider: widget.hostListProvider,
+                      guestListProvider: widget.guestListProvider)));
         },
         child: Icon(Icons.add),
       ),
     );
   }
 }
-
