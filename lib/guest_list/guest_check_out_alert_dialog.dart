02cc4016028ad5b136aabd1d9dd9a48fbdfbbdd3
”SulingXu“ <xusuling2020@gmail.com>
2021-08-20 19:45:43 +0200

    Update money calculating function

--- lib/guest_list/guest_check_out_alert_dialog.dart
+++ lib/guest_list/guest_check_out_alert_dialog.dart
@@ -3,10 +3,14 @@ import 'package:registry/guest_list/guest.dart';
 import 'package:registry/styles.dart';
 import 'package:registry/guest_list/guest_fee_paying.dart';
 import 'package:registry/guest_list/guest_status.dart';
+import 'package:registry/host_list/host_list_provider.dart';
+import 'package:registry/guest_list/guest_list_provider.dart';
 
 class GuestCheckOutAlertDialog extends StatefulWidget {
-  GuestCheckOutAlertDialog({Key? key, required this.guest, required this.checkOutTimeUpdate}) : super(key: key);
+  GuestCheckOutAlertDialog({Key? key, required this.guest, required this.checkOutTimeUpdate, required this.guestListProvider, required this.hostListProvider}) : super(key: key);
   Guest guest;
+  final GuestListProvider guestListProvider;
+  final HostListProvider hostListProvider;
   VoidCallback checkOutTimeUpdate;
   @override
   _GuestCheckOutAlertDialogState createState() => _GuestCheckOutAlertDialogState();
@@ -19,13 +23,14 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
   final String _questionTxt = 'Are you sure to check out?';
   final String _wrongTimewarning = 'Wrong registry time';
   final double _playingFeePerHour = 50.0;
+  final double _playingFeePerMinute = 1.0;
 
   String _calTotalTime(DateTime? startTime, DateTime? endTime) {
     if(startTime == null || endTime == null ) {
       return _wrongTimewarning;
     }
-    double totalTime = (endTime.hour + (endTime.minute / 60)) -
-        (startTime.hour + (startTime.minute / 60));
+    double totalTime = (endTime.day * 24 + endTime.hour + (endTime.minute / 60)) -
+        (startTime.day * 24 + startTime.hour + (startTime.minute / 60));
     int hours = totalTime.floor();
     int minuts = ((totalTime - totalTime.floorToDouble()) * 60).round();
     return (hours == 0)
@@ -33,14 +38,14 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
         : (minuts == 0) ? '${hours}hr' : '${hours}hr ${minuts}min';
   }
 
-  double _calTotalMoney(String calTotalTime, double playingFeePerHour) {
+  double _calTotalMoney(String calTotalTime, double playingFeePerMinute) {
     if (calTotalTime.contains("hr") && calTotalTime.contains("min")) {
-      return (int.parse(calTotalTime.split('hr')[0]) + 1) * playingFeePerHour; // n hr m min
+      return (int.parse(calTotalTime.split('hr')[0]) * 60 + int.parse(calTotalTime.split(' ')[1][0]))* playingFeePerMinute; // n hr m min
     } else if(calTotalTime.contains("hr")) {
-      return int.parse(calTotalTime.split('hr')[0]) * playingFeePerHour;// = n hr
+      return int.parse(calTotalTime.split('hr')[0]) * playingFeePerMinute * 60;// = n hr
     } else if(calTotalTime.contains("min")) {
       if (int.parse(calTotalTime.split('min')[0]) !=0) {
-        return playingFeePerHour;// 1 min < * < 1 hr
+        return int.parse(calTotalTime.split('min')[0]) * playingFeePerMinute ;// 1 min < nmin < 1 hr
       } else { // 0 min
         return 0;
       }
@@ -49,14 +54,14 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
     }
   }
 
-  void _navigateToDifferentPages(BuildContext context, double fee) {
+  void _navigateToDifferentPages(BuildContext context, double fee, HostListProvider hostListProvider, GuestListProvider guestListProvider) {
     if (fee != 0) {
       Navigator.push(
         context,
-        new MaterialPageRoute<void>(builder: (context) => GuestFeePaying(guestFee: widget.guest.fee)),
+        new MaterialPageRoute<void>(builder: (context) => GuestFeePaying(guestFee: fee, hostListProvider: hostListProvider, guestListProvider: guestListProvider)),
       );
     } else {
-      Navigator.popUntil(context, (Route<dynamic> route) => route.isFirst);
+      Navigator.of(context).pop();
     }
   }
 
@@ -64,17 +69,18 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
     return TextButton(
       child: Text(_cancelButtonTxt),
       onPressed:  () {
+        widget.guest.checkOutTime = null;
         Navigator.of(context).pop();
       },
     );
   }
 
-  Widget continueButton (BuildContext context, double fee) {
+  Widget continueButton (BuildContext context, double fee, HostListProvider hostListProvider, GuestListProvider guestListProvider) {
     return TextButton(
       child: Text(_continueButtonTxt),
       onPressed:  () {
         widget.checkOutTimeUpdate();
-        _navigateToDifferentPages(context, fee);
+        _navigateToDifferentPages(context, fee, hostListProvider, guestListProvider);
       },
     );
   }
@@ -85,11 +91,11 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
     String _status = (widget.guest.status == GuestStatus.play) ? 'play' : 'watch';
     String _totalStayTime = _calTotalTime(widget.guest.checkInTime, widget.guest.checkOutTime);
     String _showNameTimeStatus = _guestName + ' spent ' + _totalStayTime + ' ' + _status + 'ing,';
-    widget.guest.fee = _calTotalMoney(_totalStayTime, _playingFeePerHour);
-    String _showMoney = (_status == 'play' && widget.guest.fee != 0) ? "need to pay " + widget.guest.fee.toString() + "SEK" : "don't need to pay";
+    widget.guest.fee = (_status == 'play') ? _calTotalMoney(_totalStayTime, _playingFeePerMinute) : 0;
+    String _showMoney = (_status == 'play' && widget.guest.fee != 0) ? "need to pay " + widget.guest.fee.toString() + " SEK" : "don't need to pay";
 
     return AlertDialog(
-      title: Text(_alertDialogTitle),
+      title: Center(child:Text(_alertDialogTitle)),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -100,7 +106,7 @@ class _GuestCheckOutAlertDialogState extends State<GuestCheckOutAlertDialog> {
       ),
       actions: [
         cancelButton(context),
-        continueButton(context, widget.guest.fee),
+        continueButton(context, widget.guest.fee, widget.hostListProvider, widget.guestListProvider),
       ],
     );
   }
