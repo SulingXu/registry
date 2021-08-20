96ff375dfe928722aed8e6e789df71f4c952d65e
”SulingXu“ <xusuling2020@gmail.com>
2021-08-20 19:44:54 +0200

    Separate the CheckInButtonWidget from GuestInforInputWidget

--- lib/guest_list/guest_info_input_widget.dart
+++ lib/guest_list/guest_info_input_widget.dart
@@ -1,100 +1,99 @@
 import 'package:flutter/material.dart';
-import 'guest_status_widget.dart';
-import 'guest.dart';
-import 'guest_list_widget.dart';
-import 'guest_list_provider.dart';
+import 'package:flutter/services.dart';
+import 'package:registry/guest_list/guest_check_in_button_widget.dart';
+import 'package:registry/guest_list/guest_status_widget.dart';
+import 'package:registry/guest_list/guest.dart';
+import 'package:registry/guest_list/guest_list_provider.dart';
+import 'package:registry/host_list/host_list_provider.dart';
+import 'package:registry/guest_list/guest_status.dart';
 import '../styles.dart';
 
 class GuestInfoInputWidget extends StatefulWidget {
-  const GuestInfoInputWidget({Key? key, required this.chosenHostName, required this.guestListProvider}) : super(key: key);
+  const GuestInfoInputWidget({Key? key, required this.chosenHostName, required this.guestListProvider, required this.hostListProvider}) : super(key: key);
   final String chosenHostName;
   final GuestListProvider guestListProvider;
+  final HostListProvider hostListProvider;
 
   @override
   _GuestInfoInputWidgetState createState() => _GuestInfoInputWidgetState();
 }
 
 class _GuestInfoInputWidgetState extends State<GuestInfoInputWidget> {
+  final _formKey = GlobalKey<FormState>();
   final _guestLastNameController = TextEditingController();
   final _guestFirstNameController = TextEditingController();
+  final String _guestInfoTitle = 'Guest Information';
   final String _firstNameTextPrefix = 'First Name:';
   final String _lastNameTextPrefix = 'Last Name:';
-  final String _checkInTxt = 'Check in';
-  final String _firstNameInputHintText = 'Enter your first name';
-  final String _lastNameInputHintText = 'Enter your last name';
-  final String _alertDialogTxt = 'The information cannot be left empty! Please input or choose all of them.';
-  Guest _guest = Guest();
+  final String _firstNameInputHintText = 'Enter your first name' + ' (Only letters are allowed)';
+  final String _lastNameInputHintText = 'Enter your last name' + ' (Only letters are allowed)';
+  final String _emptyInputAlertTxt = 'Please enter some text';
+  final String _onlySpaceInputAlertTxt = 'Please enter a valid name';
+  Guest _guest = Guest('', '', null, GuestStatus.play, null, null, 0, false);
 
-  Widget textFormField(TextEditingController controller, String hintText) {
+  Widget textFormField(TextEditingController controller, String hintText, String emptyInputAlert, String onlySpaceInputAlert) {
     return TextFormField(
       controller: controller,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]")),
+      ],
       decoration: InputDecoration(
         border: UnderlineInputBorder(),
         hintText: hintText,
       ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return emptyInputAlert;
+        } else if (value.replaceAll(RegExp(' +'), ' ') == ' ') {
+          return onlySpaceInputAlert;
+        }
+        return null;
+      },
     );
   }
   Widget nameInputColumn(String prefix, TextEditingController controller, String hintText){
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Styles.text(prefix),
+        Styles.text(prefix, Styles.middleTextWithDefaultColor),
         Container(
-          child: textFormField(controller, hintText),
+          child: textFormField(controller, hintText, _emptyInputAlertTxt, _onlySpaceInputAlertTxt),
         ),
       ],
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Guest Information"),
+        title: Text(_guestInfoTitle),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: nameInputColumn(_firstNameTextPrefix, _guestFirstNameController, _firstNameInputHintText),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: nameInputColumn(_lastNameTextPrefix, _guestLastNameController, _lastNameInputHintText),
-          ),
-          GuestStatusWidget(guest: _guest),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: ElevatedButton(
-              style: Styles.buttonStyle,
-              onPressed: _onPressed,
-              child: Text(_checkInTxt),
-            )
-          ),
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: nameInputColumn(_firstNameTextPrefix, _guestFirstNameController, _firstNameInputHintText),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: nameInputColumn(_lastNameTextPrefix, _guestLastNameController, _lastNameInputHintText),
+            ),
+            GuestStatusWidget(guest: _guest),
+            GuestCheckInButtonWidget(hostListProvider: widget.hostListProvider,
+                formKey: _formKey,
+                guestListProvider: widget.guestListProvider,
+                guest: _guest,
+                chosenHostName: widget.chosenHostName,
+                guestLastNameController: _guestLastNameController,
+                guestFirstNameController: _guestFirstNameController,
+            ),
+          ],
+        )
       )
     );
-  }
-  void _onPressed() {
-    if (_guestFirstNameController.text != '' && _guestLastNameController.text != '' && _guest.status != null) {
-      _guest.checkInTime = DateTime.now();
-      _guest.chosenHostName = widget.chosenHostName;
-      _guest.lastName = _guestLastNameController.text;
-      _guest.firstName = _guestFirstNameController.text;
-      widget.guestListProvider.addGuest(_guest);
-      Navigator.pushReplacement(
-          context,
-          new MaterialPageRoute<void>(builder: (context) => GuestListWidget(guestListProvider: widget.guestListProvider))
-      );
-    } else {
-      showDialog<void>(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Text(_alertDialogTxt),
-          );
-        },
-      );
-    }
   }
 }
